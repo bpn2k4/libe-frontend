cc58def4535c4ba3e09b1682cd2e28c9d089f11e
bpn2k4 <bpn2k4@gmail.com>
2024-01-28 15:26:53 +0700

    Add button on Group image home

--- src/components/GroupPictureHome.jsx
+++ src/components/GroupPictureHome.jsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom"
 import { IconImagePlaceHolder } from "./Icon"
+import { twMerge } from "tailwind-merge"
 
 export const GroupPictureHome = () => {
 
@@ -13,6 +15,8 @@ export const GroupPictureHome = () => {
         <div className="absolute overflow-hidden skeleton offset-0 center ">
           <IconImagePlaceHolder className='z-[2]' />
         </div>
+        <RoundedButton
+          title='Shop now' />
       </div>
       <div className='w-full md:w-1/2 aspect-1/2'>
         <div className='w-full aspect-square relative'>
@@ -24,6 +28,7 @@ export const GroupPictureHome = () => {
           <div className='absolute skeleton offset-0 center'>
             <IconImagePlaceHolder className='z-[1]' />
           </div>
+
         </div>
         <div className='w-full aspect-square flex flex-row'>
           <div className='flex-1 relative'>
@@ -49,5 +54,20 @@ export const GroupPictureHome = () => {
         </div>
       </div>
     </div>
+  )
+}
+
+const RoundedButton = ({ className, title, to = '/' }) => {
+
+  return (
+    <Link
+      className={twMerge(
+        'absolute z-[5] left-4 bottom-4 text-2xl py-2 px-4 border-2 border-white rounded-full active:scale-95 transition-transform text-white overflow-hidden group',
+        className
+      )}
+      to={to}>
+      {title}
+      <span className='absolute offset-0 bg-black z-[-1] scale-x-0 group-hover:scale-x-100 transition-transform'></span>
+    </Link>
   )
 }
